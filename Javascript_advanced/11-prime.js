d1d438de247b9ade1e9e5c93b7e356bd866634e4
Chris Stamper <cstampok@gmail.com>
2023-11-10 15:38:31 -0600

    eleven test success

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,32 @@
+function isPrime(num) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+function countPrimeNumbers() {
+  let count = 0;
+  for (let i = 2; i <= 100; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const start = performance.now();
+
+for (let i = 0; i < 100; i++) {
+  setTimeout(countPrimeNumbers, 0);
+}
+
+const end = performance.now();
+
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${
+    end - start
+  } milliseconds.`
+);
