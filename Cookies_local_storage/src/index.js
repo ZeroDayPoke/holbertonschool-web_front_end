9e62bd30fa059a2da739eb2d2fae3d71765087a1
Chris Stamper <cstampok@gmail.com>
2023-11-17 13:02:41 -0600

    one qualified success

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,8 +1,12 @@
 function setCookies() {
   const firstname = document.getElementById("firstname").value;
   const email = document.getElementById("email").value;
-  document.cookie = "firstname=" + firstname;
-  document.cookie = "email=" + email;
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 10);
+  const expires = "expires=" + expirationDate.toUTCString();
+  const path = "path=/";
+  document.cookie = "firstname=" + firstname + "; " + expires + "; " + path;
+  document.cookie = "email=" + email + "; " + expires + "; " + path;
 }
 
 function showCookies() {
